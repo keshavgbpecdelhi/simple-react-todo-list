282eecb7f6422524b5757265d505c6ad71daaacc
Keshav <56075233+keshavgbpecdelhi@users.noreply.github.com>
2022-03-22 10:21:14 +0530

    Update List.js

--- src/List.js
+++ src/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const List = ({ items }) => (
   <ul>
-    <h1>Hello</h1>
+    <h1>Hello List</h1>
     {
       items && items.map((item, index) => <li key={index}>{item}</li>)
     }
